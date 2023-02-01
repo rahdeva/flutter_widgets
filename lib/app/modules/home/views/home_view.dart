bd9274968873c7881f73f0f55cf191f0e56b48c9
Ngurah Deva <wirandanaputra@gmail.com>
2023-02-01 23:12:55 +0800

    edit files

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,21 +1,69 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
+import 'package:sizer/sizer.dart';
 
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
+  const HomeView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('HomeView'),
-        centerTitle: true,
+        title: const Text('Flutter'),
       ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      body: Container(
+        width: 100.w * 0.86,
+        height: 100,
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+                padding: const EdgeInsetsDirectional.fromSTEB(15, 0, 0, 0),
+                width: 60,
+                height: 60,
+                child: const Icon(
+                  Icons.calendar_month,
+                )),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(10, 15, 10, 0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Book Appointment',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        color: Colors.orange,
+                        fontSize: 19,
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 8),
+                        child: AutoSizeText(
+                          'Schedule an appointment with our licensed professional.',
+                          style: TextStyle(
+                            fontFamily: 'Outfit',
+                            color: Color(0xB4FFFFFF),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -20,74 +19,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Sizer(builder: (context, orientation, deviceType) {
-      return GetMaterialApp(
-        useInheritedMediaQuery: true,
-        builder: DevicePreview.appBuilder,
-        debugShowCheckedModeBanner: false,
-        title: 'Every Flutter Widget',
-        initialRoute: AppPages.INITIAL,
-        getPages: AppPages.routes,
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Flutter'),
-          ),
-          // body: const Text('Flutter'),
-          body: Container(
-            width: 100.w * 0.86,
-            height: 100,
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Container(
-                  padding: const EdgeInsetsDirectional.fromSTEB(15, 0, 0, 0),
-                  width: 60,
-                  height: 60,
-                  child: const Icon(
-                    Icons.calendar_month,
-                  )
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsetsDirectional.fromSTEB(10, 15, 10, 0),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
-                          'Book Appointment',
-                          textAlign: TextAlign.start,
-                          style: TextStyle(
-                            color: Colors.orange,
-                            fontSize: 19,
-                          ),
-                        ),
-                        Expanded(
-                          child: Padding(
-                            padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 8),
-                            child: AutoSizeText(
-                              'Schedule an appointment with our licensed professional.',
-                              style: TextStyle(
-                                fontFamily: 'Outfit',
-                                color: Color(0xB4FFFFFF),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      );
-    });
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return GetMaterialApp(
+          useInheritedMediaQuery: true,
+          builder: DevicePreview.appBuilder,
+          debugShowCheckedModeBanner: false,
+          title: 'Every Flutter Widget',
+          initialRoute: AppPages.INITIAL,
+          getPages: AppPages.routes,
+        );
+      }
+    );
   }
 }
