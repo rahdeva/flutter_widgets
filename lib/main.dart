4662c6bc96649c8ee4836274e1bdf890e08a98ab
Ngurah Deva <wirandanaputra@gmail.com>
2023-02-01 20:50:37 +0800

    add device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_widgets/every_widgets/about_dialog.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -10,13 +19,68 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      useInheritedMediaQuery: true,
+      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
       title: 'Every Flutter Widget',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter'),
         ),
-        body: const Text('Flutter'),
+        // body: const Text('Flutter'),
+        body: Container(
+          width: double.infinity * 0.86,
+          height: 100,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Container(
+                padding: const EdgeInsetsDirectional.fromSTEB(15, 0, 0, 0),
+                width: 60,
+                height: 60,
+                child: Icon(
+                  Icons.calendar_month,
+                )
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsetsDirectional.fromSTEB(10, 15, 10, 0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Book Appointment',
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          color: Colors.orange,
+                          fontSize: 19,
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 8),
+                          child: AutoSizeText(
+                            'Schedule an appointment with our licensed professional.',
+                            style: TextStyle(
+                              fontFamily: 'Outfit',
+                              color: Color(0xB4FFFFFF),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
